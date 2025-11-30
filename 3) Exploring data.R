a59a2206e59d865059fdd82b6a09a9f31e8c8c1a
Carol Pillai <pillaicarolcs242549@gmail.com>
2025-11-30 23:21:23 +0530

    Add files via upload

---
+++ 3) Exploring data.R
@@ -0,0 +1,28 @@
+library(readr)
+library(psych)
+
+my_data <- read.csv("C:/Users/Ziven/Downloads/Housing.csv")
+
+colnames(my_data) <- c(
+  "price",
+  "area",
+  "bedrooms",
+  "bathrooms",
+  "stories",
+  "mainroad",
+  "guestroom",
+  "basement",
+  "hotwaterheating",
+  "airconditioning",
+  "parking",
+  "prefarea",
+  "furnishingstatus"
+)
+
+head(my_data)
+
+summary(my_data)
+
+str(my_data)
+
+describe(my_data)
