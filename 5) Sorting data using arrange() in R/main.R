c2804612b4bccb5ca7c8a9cbc9e0cca23d71bf08
Carol Pillai <pillaicarolcs242549@gmail.com>
2025-11-30 22:47:04 +0530

    Add files via upload

---
+++ 5) Sorting data using arrange() in R/main.R
@@ -0,0 +1,26 @@
+library(dplyr)
+
+boston <- read.csv("C:/Users/Ziven/Downloads/Housing.csv")
+
+boston_sorted_medv <- boston |>
+  arrange(price)
+
+head(boston_sorted_medv, 5)
+
+boston_sorted_crim_desc <- boston |>
+  arrange(desc(area))
+
+head(boston_sorted_crim_desc, 5)
+
+boston_multi_sort <- boston |>
+  arrange(bedrooms, desc(price))
+
+head(boston_multi_sort, 10)
+
+large_homes_by_tax <- boston |>
+  filter(bathrooms > 3) |>
+  arrange(area)
+
+cat("Top 5 largest homes by lowest area:\n")
+print(large_homes_by_tax |> select(area, bathrooms, price) |> head(5))
+
