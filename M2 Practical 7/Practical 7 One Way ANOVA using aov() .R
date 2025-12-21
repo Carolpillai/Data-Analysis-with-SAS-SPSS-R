f1f46a5498375f34d38d3a4dbb931f1a30c9e238
Carol Pillai <pillaicarolcs242549@gmail.com>
2025-12-21 16:46:42 +0530

    Add files via upload
    prac 7 from module 2

---
+++ M2 Practical 7/Practical 7 One Way ANOVA using aov() .R
@@ -0,0 +1,11 @@
+# Load data set
+student_data <- read.csv("C:/Users/Ziven/Downloads/Student_Performance.csv")
+
+# Convert school_type to factor
+student_data$school_type <- as.factor(student_data$school_type)
+
+# One-way ANOVA
+anova_result <- aov(study_hours ~ school_type, data = student_data)
+
+# Display ANOVA table
+summary(anova_result)
